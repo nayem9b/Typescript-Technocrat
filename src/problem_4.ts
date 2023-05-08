7bce9f9c7e4c543cdc4e302e2208f157f6ebd92f
nayem9b <razwanniam1@gmail.com>
2023-05-08 09:24:45 +0600

    problem 4 halfway donw

--- src/problem_4.ts
+++ src/problem_4.ts
@@ -1,4 +1,21 @@
-const students = <T>(...param: T[]) => {
-  console.log(param.reverse());
-};
-console.log(students<string>("Rahim", "karim"));
+class Person {
+  private name: string;
+  private age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  get Name(): string {
+    return this.name;
+  }
+  get Age(): number {
+    return this.age;
+  }
+  getDetails() {
+    console.log(`Here is ${this.name} with the age of ${this.age}`);
+  }
+}
+
+const learner = new Person("Rezwan Niam", 23);
+learner.getDetails();
